32f570cf955b97baee02d33e97bc610f1ea2d7ef
gustavo1020 <49031933+gustavo1020@users.noreply.github.com>
2023-12-10 20:50:25 -0300

    Initial commit

---
+++ Context/AmarteDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Amarte.Context
+{
+    public class AmarteDbContext : IdentityDbContext
+    {
+        public AmarteDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
